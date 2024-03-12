2b3215b50e673c80708475515ecc9d6e136dddc6
lkehl247 <153117528+lkehl247@users.noreply.github.com>
2024-03-12 16:22:40 -0600

    Update Hamburger Door Dash.py

--- Hamburger Door Dash.py
+++ Hamburger Door Dash.py
@@ -9,36 +9,37 @@ class Order():
     def __init__(self):
         self.burger_count = self.randomBurgers()
 
-    def randomBurgers(): # outputs a random integer from 1 - 20
+    def randomBurgers(self):  # Added self parameter
         return random.randint(1, 20)
-      
     
+
 class Person():
     def __init__(self):
         self.customer_name = self.randomName()
 
-    def randomName(): # outputs a random name for  the lsCustomers list
+    def randomName(self):  # Added self parameter
         lsCustomers = ["Jefe", "El Guapo", "Lucky Day", "Ned Nederlander", "Dusty Bottoms", "Harry Flugleman", "Carmen", "Invisible Swordsman", "Singing Bush"]
         return random.choice(lsCustomers)
     
 
 class Customer(Person):
     def __init__(self):
-        super.__init__()
+        super().__init__()  # Added parentheses after super
         self.order = Order()
 
 
 dictCustomer = {}
 queueCustomers = []
 
-for customer in range(100):
+for _ in range(100):  # Use _ as a placeholder for loop variable
     customer = Customer()
     queueCustomers.append(customer)
 
-    if customer.customer_name in customer.dictCustomer:
-        customer.dictCustomer[customer.customer_name] += customer.order.burger_count
+    if customer.customer_name in dictCustomer:  # Use global dictCustomer
+        dictCustomer[customer.customer_name] += customer.order.burger_count
     else:
-        customer.dictCustomer[customer.customer_name] = customer.order.burger_count
-        
-listSortedCustomers = sorted(dictCustomers.items(), key=lambda x: x[1], reverse=True) 
+        dictCustomer[customer.customer_name] = customer.order.burger_count
+
+queueCustomers = sorted(dictCustomer.items(), key=lambda x: x[1], reverse=True)
+print()
 
